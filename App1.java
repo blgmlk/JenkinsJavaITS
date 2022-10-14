542b211a8f96726e8b657d3c0a89a2fadd2b871b
blgmlk <112402926+blgmlk@users.noreply.github.com>
2022-10-14 14:15:56 +0200

    Update App1.java

--- App1.java
+++ App1.java
@@ -1,5 +1,5 @@
 public class App1{
      public static void main(String[] args){
-        System.out.println("retour sur jenkins hook");
+        System.out.println("retour sur jenkins  3hook");
     }
 }
